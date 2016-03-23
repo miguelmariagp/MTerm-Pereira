23fdb8643da59d714062b298c463b186ef812e3f
miguelmariagp <m.pereira@wustl.edu>
2016-03-23 17:40:11 -0500

    Updating documentation
    Over time

---
+++ integrateIt/R/integrateIt-package.R
@@ -0,0 +1,9 @@
+#' integrateIt
+#'
+#' The integrateIt package allows the user to compute integrations using the trapezoidal or Simpson's rule,
+#' and to calculate the number of segments in which one needs to divide the function in order to get a marginally correct result.
+#' @name integrateIt
+#' @docType package
+#' @author  Miguel Pereira: \email{m.pereira@@wustl.edu}
+#'
+NULL
