b3cd4a5415ec12065907a0a67014be8fd38ba30c
miguelmariagp <m.pereira@wustl.edu>
2016-03-23 12:03:27 -0500

    Updated Dev file
    Starting to work on examples of the functions

--- DevMid.R
+++ DevMid.R
@@ -5,9 +5,12 @@ library(roxygen2)
 
 setwd("C:/Users/ststest/Dropbox/Spr16/Programming/MTerm-Pereira") #This will need to be changed to match your directory
 
-create("integrateIt")
+#create("integrateIt")
 
 current.code <- as.package("integrateIt")
 load_all(current.code)
 document(current.code)
+
+tolTest(sqrt,1,2)
+tolTest(sqrt,1,2,Rule="Simp")
 
