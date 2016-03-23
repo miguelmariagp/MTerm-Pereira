bb5460b172504b3f7c9b6e2e69307aa271645593
miguelmariagp <m.pereira@wustl.edu>
2016-03-23 13:15:22 -0500

    Correct Trapezoid methods
    In order to allow the vector X to have a range different from a and b.
    In other words, now I can feed in a vector X from 0 to 100 and compute
    the integral from 0 to 50.

--- DevMid.R
+++ DevMid.R
@@ -16,7 +16,9 @@ x<-seq(0,2,by=.2)
 y=x^2
 
 #Creating a Trapezoid object from scratch
-tr<-new("Trapezoid",x,y,0,2)
+tr<-new("Trapezoid",x,y,0,1)
+#Using the integratIt method
+tr<-integrateIt(X=x,Y=y,a=0,b=2,Rule="Trap")
 #Using the show method
 show(tr)
 #Using the print method
@@ -25,20 +27,55 @@ print(tr)
 
 #Creating a Simspon object from scratch
 sp<-new("Simpson",x,y,0,2)
+#Using the integratIt method
+sp<-integrateIt(X=x,Y=y,a=0,b=2,Rule="Simp")
 #Using the show method
 show(sp)
 #Using the print method
 print(sp)
 
 
-#Using the integratIt method
-tr<-integrateIt(X=x,Y=y,a=0,b=1,Rule="Trap")
 
-#Using the plot method
+
+#Using the plot method for Trapezoid objects
 plot(tr)
 
 
-tolTest(sqrt,1,2)
 
-tolTest(sqrt,1,2,Rule="Simp")
+#PLOT METHOD FOR SIMPSON objects
+length(x)
+
+subvec<-which(x>=0 & x<=2)
+#And sorting them by x
+sorted<-sort.int(x[subvec], index.return=T)$ix
+x.vec<-x[sorted]
+y.vec<-y[sorted]
+
+n<-length(x[x>=0&x<=2])
+h<-(2-1)/n
+
+midpoint<-seq(2,n,2)
+v<-x.vec[midpoint]
+extremes<-cbind(x.vec,y.vec)[-midpoint,]
+
+
+
+
+top<-function(x,y){
+  n<-length(x)
+  
+  v<-(x[1]+x[2])/2
+  X<-seq(min(x),max(x),by=.01)
+  px<-y[1]*(((X-v)*(X-x[n]))/((x[1]-v)*(x[1]-x[n])))+
+    y[v]*(((X-x[1])*(X-x[n]))/((v-x[1])*(v-x[n])))+
+    y[n]*(((X-x[1])*(X-v))/((x[n]-x[1])*(x[n]-v)))
+  plot(x,y)
+  lines(px)
+}
+
+
+#Testing method tolTest
+f<-function(x) x^2
+tolTest(f,0,2)
+tolTest(f,0,2,Rule="Simp")
 
--- integrateIt/R/Trapezoid.R
+++ integrateIt/R/Trapezoid.R
@@ -36,15 +36,22 @@ setClass(Class="Trapezoid",
 #' @export
 setMethod("initialize", "Trapezoid", 
           function(.Object, X, Y, a, b){
-            .Object@X <- X
-            .Object@Y <- Y
+            
             .Object@a <- a
             .Object@b <- b
             
-            sorted<-sort.int(X, index.return=T)$ix
+            #Subsetting the vectors between a and b
+            subvec<-which(X>=a&X<=b)
+            #And sorting them
+            sorted<-sort.int(X[subvec], index.return=T)$ix
             x<-X[sorted]
             y<-Y[sorted]
-            n<-length(x[x>=a&x<=b])
+            
+            .Object@X <- x
+            .Object@Y <- y
+            
+            
+            n<-length(x)
             h<-(b-a)/n
             
             .Object@Int <- h/2*(y[1]+2*sum(y[2:n-1])+y[n])
@@ -59,6 +66,17 @@ setMethod(f="show",
           # Class the method is used for
           signature="Trapezoid",
           definition=function(object){
+            
+            #Subsetting the vectors between a and b in order to show only those used to estimate the integral
+            subvec<-which(object@X>=object@a & object@X<=object@b)
+            #And sorting them
+            sorted<-sort.int(object@X[subvec], index.return=T)$ix
+            x<-object@X[sorted]
+            y<-object@Y[sorted]
+            
+            object@X <- x
+            object@Y <- y
+            
             showtrap <- list(Values=cbind(X=object@X, Y=object@Y), Integral=object@Int)
             print(showtrap)
           }   
@@ -77,9 +95,15 @@ setMethod(f="plot",
           signature="Trapezoid",
           # The method itself
           definition=function(x=NULL, y=x, ...){
-            sorted<-sort.int(x@X, index.return=TRUE)$ix
+            
+            #Subsetting the vectors between a and b in order to plot only 
+            #those used to estimate the integral
+            subvec<-which(x@X>=x@a & x@X<=x@b)
+            #And sorting them by x
+            sorted<-sort.int(x@X[subvec], index.return=T)$ix
             x.vec<-x@X[sorted]
             y.vec<-x@Y[sorted]
+            
             plot(x.vec,y.vec,pch=16,main="Graphical display of trapezoidal rule",
                  ylim=c(0,max(y.vec)),ylab="f(X)", xlab="X")
             n<-length(x.vec)
